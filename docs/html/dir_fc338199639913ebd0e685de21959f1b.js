a375965eed52d12c83976d752c923800629ff5ce
Travis CI <travis@travis-ci.org>
2019-05-01 16:56:08 +0000

    Deploy code docs to GitHub Pages Travis build: 128
    Commit: db73adf3309ab26c3279891b40ecd916199d7ae0

--- docs/html/dir_fc338199639913ebd0e685de21959f1b.js
+++ docs/html/dir_fc338199639913ebd0e685de21959f1b.js
@@ -1,9 +1,9 @@
 var dir_fc338199639913ebd0e685de21959f1b =
 [
-    [ "20190429211125_Initial.cs", "a00016.html", [
+    [ "20190501164124_Initial.cs", "a00016.html", [
       [ "Initial", "a00003.html", "a00003" ]
     ] ],
-    [ "20190429211125_Initial.Designer.cs", "a00017.html", [
+    [ "20190501164124_Initial.Designer.cs", "a00017.html", [
       [ "Initial", "a00003.html", "a00003" ]
     ] ],
     [ "ScientificReportDbContextModelSnapshot.cs", "a00018.html", [
--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['20190429211125_5finitial_2ecs',['20190429211125_Initial.cs',['../a00016.html',1,'']]],
-  ['20190429211125_5finitial_2edesigner_2ecs',['20190429211125_Initial.Designer.cs',['../a00017.html',1,'']]]
+  ['20190501164124_5finitial_2ecs',['20190501164124_Initial.cs',['../a00016.html',1,'']]],
+  ['20190501164124_5finitial_2edesigner_2ecs',['20190501164124_Initial.Designer.cs',['../a00017.html',1,'']]]
 ];
